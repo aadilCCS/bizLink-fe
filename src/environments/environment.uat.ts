0751261b3a07323139e9ae24b122ebf0d0f75b09
brijesh-compughost <ccs.php.dev010@gmail.com>
2025-09-22 18:10:09 +0530

    GOOGLE_KEY add

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -9,6 +9,7 @@ export const environment = {
     appVersion: require('../../package.json').version,
 
     API_URL: 'https://staging-api.thebizLink.com/v1/',
+    GOOGLE_KEY: '918738585769-3kedsedav5g2hbk669o7g2m14n7q59ad.apps.googleusercontent.com',
     recaptcha: {
         siteKey: '6LdQfBArAAAAAPvXmmM7f0-77xg4DnIibT_qJ7Mm'
     }
